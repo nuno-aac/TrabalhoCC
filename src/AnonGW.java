a433692c2bc535a971e163faa5527a6a12214106
CoelhoLado <naac.1999@gmail.com>
2020-05-13 14:25:09 +0100

    Threaded environment added

--- src/AnonGW.java
+++ src/AnonGW.java
@@ -1,31 +1,57 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class AnonGW {
-    String clientInput;
     private ServerSocket welcomeSocket;
     private Socket goodbyeSocket;
+    private String targetServer;
+    ArrayList<String> peers;
+    ArrayList<Thread> workers;
+    private static final int PORT_NUM = 80;
 
-    public AnonGW(){
-        try {
-            welcomeSocket = new ServerSocket(12345);
-            goodbyeSocket = new Socket("127.0.0.1", 12345);
-        } catch(IOException e){
-            System.out.println(Arrays.toString(e.getStackTrace()));
+    boolean parseArgs(String[] args){
+        targetServer = null;
+        peers = new ArrayList<>();
+        for(int i = 1; i < args.length; i++){
+            switch(args[i]){
+                case "-sv" :
+                    targetServer = args[i+1];
+                    i++;
+                    break;
+                case "-peers" :
+                    while(!args[i + 1].equals("-sv") && !args[i + 1].equals("-peers")){
+                        peers.add(args[i+1]);
+                        i++;
+                    }
+                    break;
+                default:
+                    System.out.println("Argumento desconhecido: " + args[i]);
+            }
+        }
+        return (targetServer != null && peers.size() != 0);
+    }
+
+    public AnonGW(String[] args){
+        if(parseArgs(args)) {
+            try {
+                welcomeSocket = new ServerSocket(PORT_NUM);
+                goodbyeSocket = new Socket(targetServer, PORT_NUM);
+            } catch (IOException e) {
+                System.out.println(Arrays.toString(e.getStackTrace()));
+            }
+        } else {
+            System.out.println("Argumentos inválidos!");
         }
     }
     public void gwStart() throws Exception {
-
         while (welcomeSocket != null) {
             Socket clientSocket = welcomeSocket.accept();
-            BufferedReader inFromClient =
-                    new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter outToServer = new PrintWriter(goodbyeSocket.getOutputStream());
-            clientInput = inFromClient.readLine();
-            outToServer.println("Received: " + clientInput);
+            Thread t = new Thread(new Worker(clientSocket, goodbyeSocket));
+            workers.add(t);
+            t.start();
         }
-
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        AnonGW anongw = new AnonGW();
+        AnonGW anongw = new AnonGW(args);
         try{
             anongw.gwStart();
         } catch (Exception e){
